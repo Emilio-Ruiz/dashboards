3bac5bb7df07c3567a7ea586a9a632d1f22a0ad0
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-11 16:45:37 +0000

    raw data updated

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,6 +1,10 @@
 class CurrenciesController < ApplicationController 
   def first_currency
 
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+
+    #@array_of_symbols = ...
+
     render({ :template => "currency_templates/step_one.html.erb" })
   
   end
