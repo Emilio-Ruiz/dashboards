3aab59b7043fb44b9aeb4c0a3667b64834a45335
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-11 17:32:58 +0000

    git

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -10,5 +10,8 @@ class CurrenciesController < ApplicationController
     render({ :template => "currency_templates/step_one.html.erb" })
   
   end
+  def second_currency 
+    render({ :template => "currency_templates/step_two.html.erb" })
+  end
 
 end 
