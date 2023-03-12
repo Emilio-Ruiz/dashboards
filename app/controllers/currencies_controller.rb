71b908ede2f6280bbfbeb8f3c13379ec05929e56
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-12 17:49:57 +0000

    this is title

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,7 +1,9 @@
 class CurrenciesController < ApplicationController
   def first_currency 
-  
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
     render({ :template => "currency_templates/step_one.html.erb" })
+    symbols_hash = @parsed_data.fetch("symbols")
   end
 
 end
