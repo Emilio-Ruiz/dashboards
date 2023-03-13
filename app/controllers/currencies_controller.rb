6ef6204eccaf310fc615aa47a988f4fdf0a8934d
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-13 16:52:33 +0000

    recent git

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -12,6 +12,9 @@ class CurrenciesController < ApplicationController
    @parsed_data = JSON.parse(@raw_data)
    @symbols_hash = @parsed_data.fetch("symbols")
    @array_of_symbols = @symbols_hash.keys
+
+   @from_symbol = params.fetch("from_currency")
+   
     render({:template => "currency_templates/step_two.html.erb" })
   end
 
