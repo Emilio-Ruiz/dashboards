135d3ce5df1689791a03d5b45e0cf2fcb898be58
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-13 16:17:49 +0000

    here

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   get("/forex", {:controller => "currencies", :action => "first_currency" })
 
+  get("/forex/:from_currency", {:controller => "currencies", :action => "second_currency" })
 end 
 
 
